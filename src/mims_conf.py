df244c8b8b16df2d40553da90b1c70442e194ba0
Marsh <ifrguy@users.noreply.github.com>
2017-06-17 10:39:38 -0600

    Add files via upload
    Conf now includes: host, port and db name.

--- src/mims_conf.py
+++ src/mims_conf.py
@@ -1,9 +1,11 @@
 """
 MIMS Configuration File
 """
+# Mongo DB host options
+MIMS_HOST = 'localhost'
+MIMS_PORT = 27017
+MIMS_DB = 'NHWG'
+
 # Where to save output
 LogFilePath = "./log/"
 JobFilePath = "./job/"
-
-# Delete user records from MongoDB Google DB if purged
-DeletePurged = False
