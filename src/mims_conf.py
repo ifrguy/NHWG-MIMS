95d86ee9aea424bf7386eff5da5ed91b032e06db
Marsh <ifrguy@users.noreply.github.com>
2017-06-18 17:43:50 -0500

    Update mims_conf.py

--- src/mims_conf.py
+++ src/mims_conf.py
@@ -5,6 +5,9 @@ MIMS Configuration File
 MIMS_HOST = 'localhost'
 MIMS_PORT = 27017
 MIMS_DB = 'NHWG'
+# You must set the user and password to authenticate with the DB
+MIMSUSER = ''
+MIMSPASS = ''
 
 # Where to save output
 LogFilePath = "./log/"
