25b7e411ab5ecdaa897ad2e94a2e1a0f89e260d7
Marsh <ifrguy@users.noreply.github.com>
2017-09-20 19:54:19 -0400

    Update capwatch_conf.py
    Added new config item: DL_FILEPATH fully qualified path to capwatch zipfile.

--- src/capwatch_conf.py
+++ src/capwatch_conf.py
@@ -10,8 +10,11 @@ PASSWD = ''
 # Unit default
 UNIT = 'NER-NH-001'
 
-# Timeout value for download - how long to wait for download to finish
-TIMEOUT = 50
+# Fully qualified path name for downloaded CAPWATCH zipfile
+DL_FILEPATH = ''
+
+# Timeout value for download - how long to wait for file exsits
+TIMEOUT = 5
 
 # How long to wait for an HTML DOM element to appear in webdriver
 DOM_TIMEOUT = 50
