79aa9dcfbc1bb03f173e63b310eb7ef65450a0e0
Marsh <ifrguy@users.noreply.github.com>
2018-08-12 10:49:13 -0400

    Add files via upload
    Minor aesthetic change.

--- src/capwatch2.py
+++ src/capwatch2.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env /usr/bin/python3
+#!/usr/bin/python3
 
 ## Copyright 2018 Marshall E. Giguere
 ##
@@ -16,7 +16,7 @@
 
 from capwatch2_conf import *
 from capwatch2_creds import *
-import argparse, os, sys, json, requests, base64
+import argparse, os, sys, json, requests, base64, time
 
 """
 capwatch2.py replaces capwatch.py as the preferred downloader for
@@ -27,6 +27,7 @@ use the API, or web.  Also note that you are restricted to downloading
 CAPWATCH once in a 24 hour period.
 """
 # History:
+# 10Aug18 MEG Auto retries on failure up to TRIES times.
 # 14Jan18 MEG Created.
 #
 
@@ -38,6 +39,9 @@ parser.add_argument( '-o', default=ORGID, type=int,
                      metavar='org', help='CAP organization number (' + str(ORGID) + ')')
 parser.add_argument( '-p', default=PASSWD, metavar='password',
                      help='eServices password')
+parser.add_argument( '-r', default=TRIES, type=int,
+                     metavar='retries',
+                     help='Number of times to try download (' + str(TRIES) +')')
 parser.add_argument( '-t', default=TIMEOUT, metavar='timeout',
                      help='Response time out in seconds (' + str(TIMEOUT) + ')' )
 parser.add_argument( '-v', help='Verbose', action='store_true')
@@ -56,30 +60,40 @@ args = 'ORGID={}&unitOnly=0'.format( opts.o )
 uri = url + args
 if ( opts.v ): print( 'URI:', uri )
 
-# Make download request and save results
-if ( opts.v ): print( 'Requesting CAPWATCH for OrgID:', opts.o )
+def download():
+    """
+    Attempt to download CAPWATCH file.
+    Save returned file to directory specified by caller.
+    """
+    if ( opts.v ): print( 'Requesting CAPWATCH for OrgID:', opts.o )
+    try:
+        r = requests.get( uri, auth=( opts.i, opts.p), timeout=opts.t ) 
+    except requests.execptions.HTTPError as e:
+        print( e, r.status_code, r.reason )
+        return r.status_code
+    except requests.exceptions.Timeout:
+        print('Request: download orgid:', str( opts.o ), 'timed out.')
+        return r.status_code
+    except requests.exceptions.RequestException as e:
+        print( e, r.status_code, r.reason )
+        return r.status_code
+        if ( opts.v ): print("HTTP request returned status code:", r.status_code )
+    if ( r.status_code == 200 ):
+        if ( opts.v ): print( 'Request OK downloading to:', opts.outfile )
+        with open( opts.outfile , 'wb' ) as f:
+            f.write( r.content )
+        if ( opts.v ): print( 'Download Complete.' )
+    else:
+        print( 'ERROR:', r.status_code, r.content )
+    return r.status_code
 
-try:
-    r = requests.get( uri, auth=( opts.i, opts.p), timeout=opts.t ) 
-except requests.execptions.HTTPError as e:
-    print( e, r.status_code, r.reason )
-    sys.exit( 1 )
-except requests.exceptions.Timeout:
-    print('Request: download orgid:', str( opts.o ), 'timed out.')
-    sys.exit( 1 )
-except requests.exceptions.RequestException as e:
-    print( e, r.status_code, r.reason )
-    sys.exit( 1 )
-
-if ( opts.v ): print("HTTP request returned status code:", r.status_code )
-
-if ( r.status_code == 200 ):
-    if ( opts.v ): print( 'Request OK downloading to:', opts.outfile )
-    with open( opts.outfile , 'wb' ) as f:
-        f.write( r.content )
-    if ( opts.v ): print( 'Download Complete.' )
-else:
-    print( 'ERROR:', r.status_code, r.content )
+# Make download request and save results, if fail try for RETRIES times.
+for i in range( 1, TRIES ):
+    if ( opts.v ): print( 'Try #:', i )
+    ret = download()
+    if ret == 200:
+        break
+    time.sleep( 30 )
 
 if ( opts.v ): print( 'Done.' )
-sys.exit( r.status_code )
+sys.exit( ret )
