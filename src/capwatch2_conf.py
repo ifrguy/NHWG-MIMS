7166eb252085236786bdb08a091b95f89edddd04
Marsh <ifrguy@users.noreply.github.com>
2018-08-15 12:40:10 -0400

    Update capwatch2_conf.py
    Moved timing options into code file.

--- src/capwatch2_conf.py
+++ src/capwatch2_conf.py
@@ -8,15 +8,8 @@ These options may be overridden by commandline options.
 """
 import os
 
-# Number of attempts to try and download CAPWATCH before giving up.
-TRIES = 5
-
 # Fully qualified output filename
 OUTFILE = None
 
 # The default organization to download. This is the CAP OrgId.
 ORGID = None
-
-
-# Request timeout, how long to wait for response
-TIMEOUT = 120
