6c649187d1c7a445ccd330a40436982ea2f499d7
Marsh <ifrguy@users.noreply.github.com>
2018-10-06 21:27:56 -0400

    Add files via upload
    Removed extraneous index creation commands.

--- src/MbrAchievements-index.js
+++ src/MbrAchievements-index.js
@@ -1,13 +1,4 @@
 db = db.getSiblingDB( "NHWG" );
-db.Member.createIndex( { CAPID : 1 }, { unique : 1 } );
-db.Member.createIndex( {'NameLast':1} );
-
-db = db.getSiblingDB( "NHWG" );
 db.MbrAchievements.createIndex( { CAPID : 1 } );
 
-db = db.getSiblingDB( "NHWG" );
-db.MbrAddresses.createIndex( { CAPID : 1 } );
-
-db = db.getSiblingDB( "NHWG" );
-db.MbrContact.createIndex( { CAPID : 1 } );
 
