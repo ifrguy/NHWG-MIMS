032535f1a0196ef7d2293cb30cfd2fadc33ec178
Marsh <ifrguy@users.noreply.github.com>
2019-02-14 06:59:19 -0500

    Update capwatch2_conf.py
    Moved retry and timeout control vars's into conf file from script.

--- src/capwatch2_conf.py
+++ src/capwatch2_conf.py
@@ -13,3 +13,14 @@ OUTFILE = None
 
 # The default organization to download. This is the CAP OrgId.
 ORGID = None
+
+# Default retry and connection control values
+
+# Number of attempts to download CAPWATCH file                                                                                                                  
+TRIES = 5                                                                       
+                                                                                
+# Time to wait between retries in seconts                                       
+RETRY_DELAY_TIME = 30                                                           
+                                                                                
+# Max time to wait for TCP connection to time out in seconds                    
+TIMEOUT = 120                                                                   
