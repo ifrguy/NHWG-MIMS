75ae84767d484ef94fba3ec0f0e5b4aa42b21757
Marsh <ifrguy@users.noreply.github.com>
2019-04-22 19:45:35 -0400

    Add files via upload
    New with additional qualifications added.

---
+++ src/Queries/All_OPSQuals_Senior.js
@@ -0,0 +1,125 @@
+db.getCollection("Member").aggregate(
+
+	// Pipeline
+	[
+		// Stage 1
+		{
+			$match: // Find all active senior members with active or training status qualifications.
+			{
+			    MbrStatus: 'ACTIVE',
+			    Type: 'SENIOR',
+			}
+		},
+
+		// Stage 2
+		{
+			$lookup: // Uncorrelated Subqueries
+			// (supported as of MongoDB 3.6)
+			// Join on CAPID, select only ACTIVE or TRAINING Quals,
+			// stuff them into the Quals array
+			{
+			    from: "MbrAchievements",
+			    let: { capid: "$CAPID" },
+			    pipeline: [ { $match: { $expr: { $and:[ { $eq:[ "$CAPID", "$$capid"] },{ $in: ["$AchvID",[55,56,57,61,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,124,125,126,127,128,186,189,190,193,248,]]}]}}},
+			        { $match: { $expr: {
+			            $or:[ { $eq:[ "$Status", "ACTIVE" ]},
+			            { $eq: [ "$Status", "TRAINING"]}]
+			        }}},
+			        { $project: {_id:0, AchvID:1, Status:1}}
+			        ],
+			    as: "Quals"
+			}
+		},
+
+		// Stage 3
+		{
+			$project: {
+			    CAPID:1,NameLast:1,NameFirst:1,MiddleName:1,NameSuffix:1,Type:1,Quals:1
+			}
+		},
+
+		// Stage 4
+		{
+			$unwind: {
+			    path : "$Quals",
+			    preserveNullAndEmptyArrays : false // optional
+			}
+		},
+
+		// Stage 5
+		{
+			$project: {
+			    // specifications
+			    CAPID: 1,
+			    NameLast: 1,
+			    NameFirst: 1,
+			    NameSuffix: 1,
+			    Type: 1,
+			    Qual_ID: "$Quals.AchvID",
+			    QualStat: "$Quals.Status",
+			}
+		},
+
+		// Stage 6
+		{
+			$lookup: // Equality Match Join on AchvID to pull in achievement name strings
+			{
+			    from: "Achievements",
+			    localField: "Qual_ID",
+			    foreignField: "AchvID",
+			    as: "Achievements"
+			}
+		},
+
+		// Stage 7
+		{
+			$unwind: {
+			    path : "$Achievements",
+			    preserveNullAndEmptyArrays : false // optional
+			}
+		},
+
+		// Stage 8
+		{
+			$project: {
+			    // specifications
+			    _id: 0,
+			    CAPID: 1,
+			    NameLast: 1,
+			    NameFirst: 1,
+			    NameSuffix: 1,
+			    Type: 1,
+			    Qual_ID: 1,
+			    QualStat: 1,
+			    Qualification: "$Achievements.Achv",
+			    FunctionArea: "$Achievements.FunctionalArea",
+			    
+			}
+		},
+
+		// Stage 9
+		{
+			$sort: {
+			    CAPID:1
+			}
+		},
+
+		// Stage 10
+		{
+			$out: "All_OPSQuals_Senior"
+		},
+	],
+
+	// Options
+	{
+		collation: {
+			
+			locale: "en_US",
+			strength: 1,
+			caseLevel: false,
+		}
+	}
+
+	// Created with Studio 3T, the IDE for MongoDB - https://studio3t.com/
+
+);
