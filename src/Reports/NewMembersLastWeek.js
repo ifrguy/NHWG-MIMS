1006d004d1c7fb1619ada0353839a4b6f12ab069
ifrguy <ifrguy@dexa.com>
2024-05-12 11:11:38 -0600

    Fixed bug that caused TypeError on missing contact.

--- src/Reports/NewMembersLastWeek.js
+++ src/Reports/NewMembersLastWeek.js
@@ -2,6 +2,7 @@
 // produce a csv file, with header.
 
 // History:
+// 12May24 MEG Catch missing email contact error.
 // 29Aug23 MEG "getSiblingDB" call removed, DB passed on command line.
 // 02Oct18 MEG Created.
 
@@ -16,10 +17,17 @@ print("CAPID,NameLast,NameFirst,Type,Unit,EMAIL");
 // Run query to find new members
 var cur = db.Member.find({$or: [ {Joined:{ $gte: sdate }}, { OrgJoined: {$gte: sdate }}]}).sort({Unit:1, NameLast:1});
 
+
 while ( cur.hasNext() ) {
-	var mbr = cur.next();
-	var ctct = db.MbrContact.findOne( {CAPID:mbr.CAPID,Type:'EMAIL',Priority:'PRIMARY'});
-	var ln = (mbr.NameSuffix == undefined? "" : mbr.NameLast + " " + mbr.NameSuffix);
-	print(mbr.CAPID + ',' + ln + ',' + mbr.NameFirst +
-			',' + mbr.Type + ',' + '"' + mbr.Unit + '"' + ',' + ctct.Contact);
+    var mbr = cur.next();
+    print( "CAPID:", mbr.CAPID );
+    var ctct = db.MbrContact.findOne( {CAPID:mbr.CAPID,Type:'EMAIL',Priority:'PRIMARY'});
+    var ln = (mbr.NameSuffix == undefined? "" : mbr.NameLast + " " + mbr.NameSuffix);
+    try {
+        print(mbr.CAPID + ',' + ln + ',' + mbr.NameFirst +
+    	  ',' + mbr.Type + ',' + '"' + mbr.Unit + '"' + ',' + ctct.Contact);
+    } catch (err){
+        print(mbr.CAPID + ',' + ln + ',' + mbr.NameFirst +
+    	  ',' + mbr.Type + ',' + '"' + mbr.Unit + '"' + ',' + '***UNKNOWN***');
+    }
 }
