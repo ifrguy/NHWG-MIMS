c7fd193d9368caffd28b40e892604b23c2c38e38
ifrguy <ifrguy@dexa.com>
2024-05-15 15:43:36 -0600

    New-Members-Report-Gen -n option for no upload spreadsheet and no email notification.

--- src/Reports/NewMembersLastWeek.js
+++ src/Reports/NewMembersLastWeek.js
@@ -14,22 +14,16 @@ var today = new Date();
 var sdate = dateFns.subWeeks( today, 1 );
 print("CAPID,NameLast,NameFirst,Type,Unit,EMAIL");
 // Member email address
-var email = '';
 
 // Run query to find new members
 var cur = db.Member.find({$or: [ {Joined:{ $gte: sdate }}, { OrgJoined: {$gte: sdate }}]}).sort({Unit:1, NameLast:1});
 
-
 while ( cur.hasNext() ) {
     var mbr = cur.next();
     print( "CAPID:", mbr.CAPID );
-    try {
-	email = db.MbrContact.findOne( {CAPID:mbr.CAPID,Type:'EMAIL',Priority:'PRIMARY'}).Contact;
-    }
-    catch ( e ) {
-	email = '***UNKNOWN***';
-    }
+    var ct =email = db.MbrContact.findOne( {CAPID:mbr.CAPID,Type:'EMAIL',Priority:'PRIMARY'})
+
     var ln = (mbr.NameSuffix == undefined? "" : mbr.NameLast + " " + mbr.NameSuffix);
     print(mbr.CAPID + ',' + ln + ',' + mbr.NameFirst +
-    	  ',' + mbr.Type + ',' + '"' + mbr.Unit + '"' + ',' + email);
+    	  ',' + mbr.Type + ',' + '"' + mbr.Unit + '"' + ',' + ( ct? ct.Contact : '***UNKNOWN***' ));
 }
