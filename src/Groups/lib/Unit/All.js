a71319d528a67e999d6db34e4185d4790d44a6c9
ifrguy <ifrguy@dexa.com>
2025-03-04 15:53:49 -0700

    All groups should now be created and then have the groups: seniors, cadets, and parents added as members.  Obviates the need for managing 'all' type groups.

---
+++ src/Groups/lib/Unit/All.js
@@ -0,0 +1,6 @@
+// DEPREACTED - no longer used.
+// By convention each unit's "all" group after beign created should have these
+// groups added to it: seniors, cadets, parents as members.  This obviates
+// need to keep the "all" groups updated.
+
+return
