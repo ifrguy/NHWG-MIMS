12eb0a273f12917b65fcb3f6c36b8bd149c21ca5
ifrguy <ifrguy@dexa.com>
2026-06-20 10:22:49 -0600

    Reserve Unit flag changed to const.

--- src/Groups/lib/config.js
+++ src/Groups/lib/config.js
@@ -2,4 +2,6 @@
 
 // Allow or disallow members of the reserve unit 000
 // as members of groups.  Unit 000 are non-participating members.
-var ALLOW_RESERVE_UNIT_GROUP_MEMBERS = false;
+// Set to true if you allow reserve unit members in groups.
+
+const ALLOW_RESERVE_UNIT_GROUP_MEMBERS = false;
